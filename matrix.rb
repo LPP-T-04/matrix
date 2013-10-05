e1b902b1d53f686a20cb9583f8b7e0e1ad5a4ad1
EliezerCruzSuarez <alu0100611298@ull.edu.es>
2013-10-05 13:53:53 +0100

    Añadida multiplicación de matrices

--- matrix.rb
+++ matrix.rb
@@ -4,3 +4,18 @@ a = [[2,0,1],[3,0,0],[5,1,1]]
 b = [[1,0,1],[1,2,1],[1,1,0]]
 $row = 3 # Número de filas de la matriz
 $colum = 3 # Número de columnas de la matriz
+
+def multiplicar(a,b)
+	@c = Array.new($row) { Array.new($colum) }
+	@acumulado = 0
+	$row.times do |i|
+		$colum.times do |j|
+			@acumulado = 0
+			$row.times do |k|
+				@acumulado += a[i][k] * b[k][j]
+			end
+			@c[i][j] = @acumulado
+		end
+	end
+	imprimir @c
+end
